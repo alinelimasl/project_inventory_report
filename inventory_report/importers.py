e120c79a951f7c7f26dd543c161e13dc49818cd9
Aline <aline_lima@outlook.com>
2024-04-05 17:38:58 -0300

    Requisito 4

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -1,3 +1,4 @@
+import json
 from typing import Dict, Type
 import abc
 
@@ -13,8 +14,25 @@ class Importer(abc.ABC):
         pass
 
 
-class JsonImporter:
-    pass
+class JsonImporter(Importer):
+    def import_data(self) -> list[Product]:
+        with open(self.path) as file:
+            data = json.load(file)
+
+        products = []
+        for item in data:
+            product = Product(
+                id=item["id"],
+                product_name=item["product_name"],
+                company_name=item["company_name"],
+                manufacturing_date=item["manufacturing_date"],
+                expiration_date=item["expiration_date"],
+                serial_number=item["serial_number"],
+                storage_instructions=item["storage_instructions"],
+            )
+            products.append(product)
+
+        return products
 
 
 class CsvImporter:
