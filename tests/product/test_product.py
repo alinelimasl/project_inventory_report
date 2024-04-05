08f315819f14f3f2a7fcef107fc5b2837cc4b2c8
Aline <aline_lima@outlook.com>
2024-04-05 16:18:01 -0300

    Requisito 1

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,2 +1,25 @@
+from inventory_report.product import Product
+
+
 def test_create_product() -> None:
-    raise NotImplementedError
+    data = Product(
+        id="6511",
+        product_name="ad",
+        company_name="Silveira Peixoto - EI",
+        manufacturing_date="2023-09-15",
+        expiration_date="2083-10-08",
+        serial_number="SX80 B8JR 3709 27JT 7736 LJSS 2X1R",
+        storage_instructions="Voluptatem eius quaerat veritatis "
+        "facere fuga.",
+    )
+
+    assert data.id == "6511"
+    assert data.product_name == "ad"
+    assert data.company_name == "Silveira Peixoto - EI"
+    assert data.manufacturing_date == "2023-09-15"
+    assert data.expiration_date == "2083-10-08"
+    assert data.serial_number == "SX80 B8JR 3709 27JT 7736 LJSS 2X1R"
+    assert (
+        data.storage_instructions
+        == "Voluptatem eius quaerat veritatis facere fuga."
+    )
